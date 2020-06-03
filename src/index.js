f370388a0b40a00fccc23d8f621889d4157cad72
Aditi <alonhari@newrelic.com>
2020-06-03 16:55:57 -0700

    Adding Apollo Client with manual config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 import './style.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+  }
+});
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
