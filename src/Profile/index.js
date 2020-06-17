663b26ed340a47ca67c182608422cb3bab06177c
Aditi <alonhari@newrelic.com>
2020-06-16 19:35:13 -0700

    Adding fetchMore to get more repositories

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -3,16 +3,24 @@ import gql from 'graphql-tag';
 import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
 import Loading from '../Loading';
 import ErrorMessage from '../Error';
-import { graphql } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
-  {
+  query($cursor: String) {
     viewer {
-      repositories(first: 5, orderBy: { direction: DESC, field: STARGAZERS }) {
+      repositories(
+        first: 5
+        orderBy: { direction: DESC, field: STARGAZERS }
+        after: $cursor
+      ) {
         edges {
           node {
             ...repository
           }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
         }
       }
     }
@@ -20,15 +28,25 @@ const GET_REPOSITORIES_OF_CURRENT_USER = gql`
   ${REPOSITORY_FRAGMENT}
 `;
 
-const Profile = ({ data, loading, error }) => {
-  if (error) {
-    return <ErrorMessage error={error} />;
-  }
-  const { viewer } = data;
-  if (loading || !viewer) {
-    return <Loading />;
-  }
-  return <RepositoryList repositories={viewer.repositories} />;
+const Profile = () => {
+  const { loading, error, data, fetchMore, networkStatus } = useQuery(
+    GET_REPOSITORIES_OF_CURRENT_USER,
+    {
+      notifyOnNetworkStatusChange: true,
+    }
+  );
+
+  if (networkStatus === 4) return 'Refetching!';
+  if (loading) return <Loading />;
+  if (error) return <ErrorMessage error={error} />;
+
+  return (
+    <RepositoryList
+      loading={loading}
+      repositories={data.viewer.repositories}
+      fetchMore={fetchMore}
+    />
+  );
 };
 
-export default graphql(GET_REPOSITORIES_OF_CURRENT_USER)(Profile);
+export default Profile;
