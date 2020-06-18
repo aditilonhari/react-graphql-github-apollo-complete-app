fdf11841461af39e83772ec36aec9d8a92481f6a
Aditi <alonhari@newrelic.com>
2020-06-17 19:00:26 -0700

    Adding OrganizationSearch and lifting up the state to App

--- src/App/Navigation/index.js
+++ src/App/Navigation/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import * as routes from '../../constants/routes';
+import OrganizationSearch from './OrganizationSearch';
 import './style.css';
 
-const Navigation = () => (
+const Navigation = ({
+  location: { pathname },
+  organizationName,
+  onOrganizationSearch,
+}) => (
   <header className='Navigation'>
     <div className='Navigation-link'>
       <Link to={routes.PROFILE}>Profile</Link>
@@ -11,7 +16,13 @@ const Navigation = () => (
     <div className='Navigation-link'>
       <Link to={routes.ORGANIZATION}>Organization</Link>
     </div>
+    {pathname === routes.ORGANIZATION && (
+      <OrganizationSearch
+        organizationName={organizationName}
+        onOrganizationSearch={onOrganizationSearch}
+      />
+    )}
   </header>
 );
 
-export default Navigation;
+export default withRouter(Navigation);
--- src/App/index.js
+++ src/App/index.js
@@ -8,18 +8,29 @@ import * as routes from '../constants/routes';
 import './style.css';
 
 class App extends Component {
+  state = {
+    organizationName: 'the-road-to-learn-react',
+  };
+  onOrganizationSearch = (value) => {
+    this.setState({ organizationName: value });
+  };
+
   render() {
+    const { organizationName } = this.state;
     return (
       <Router>
         <div className='App'>
-          <Navigation />
+          <Navigation
+            organizationName={organizationName}
+            onOrganizationSearch={this.onOrganizationSearch}
+          />
           <div className='App-main'>
             <Route
               exact
               path={routes.ORGANIZATION}
               component={() => (
                 <div className='App-content_large-header'>
-                  <Organization organizationName={'the-road-to-learn-react'} />
+                  <Organization organizationName={organizationName} />
                 </div>
               )}
             />
